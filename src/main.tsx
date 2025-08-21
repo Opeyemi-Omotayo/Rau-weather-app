207c027144c848a4a441043a67201d1c3a90c8ab
Opeyemi Omotayo <96225514+Opeyemi-Omotayo@users.noreply.github.com>
2025-08-21 23:00:50 +0200

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import './index.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import AppRouter from './router/index.tsx'
-import { AppStateProvider } from './contexts/appState.tsx'
+import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import AppRouter from './router/index.tsx';
+import { Analytics } from '@vercel/analytics/react';
+import { AppStateProvider } from './contexts/appState.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppStateProvider>
       <AppRouter />
+      <Analytics />
     </AppStateProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
